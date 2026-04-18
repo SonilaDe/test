c3c565906ef8357d6121a054f2090f6263bd37d7
SonilaDe <demirisonila3@gmail.com>
2026-04-18 16:18:41 +0200

    u fshi nje tekst komponent

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+   
       <Text>Hello , kete tekst e kam shtuar ne komitin e rradhes</Text>
       <StatusBar style="auto" />
     </View>
